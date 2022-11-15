56f7d07d009b1c8a5a92eead4cf56e0666b22a2b
Reika <reikasminecraft@gmail.com>
2022-11-15 18:37:45 -0500

    Update 13

--- config.lua
+++ config.lua
@@ -1,23 +1,23 @@
 Config = {}
 
-Config.tickRate = settings.startup["tick-rate"].value
+Config.tickRate = settings.startup["tick-rate"].value--[[@as integer]]
 
-Config.repeaterRange = settings.startup["repeater-range"].value
+Config.repeaterRange = settings.startup["repeater-range"].value--[[@as number]]
 
-Config.satelliteChannels = settings.startup["satellite-channels"].value
+Config.satelliteChannels = settings.startup["satellite-channels"].value--[[@as integer]]
 
-Config.satellitePacks = settings.startup["satellite-scipacks"].value
+Config.satellitePacks = settings.startup["satellite-scipacks"].value--[[@as integer]]
 
 Config.maxSignals = {}
 
 Config.maxRange = {}
 
 for i = 1,3 do
-	Config.maxSignals[i] = settings.startup["t" .. i .. "-max-signals"].value
+	Config.maxSignals[i] = settings.startup["t" .. i .. "-max-signals"].value--[[@as integer]]
 end
 
 for i = 1,2 do
-	Config.maxRange[i] = settings.startup["t" .. i .. "-range"].value
+	Config.maxRange[i] = settings.startup["t" .. i .. "-range"].value--[[@as number]]
 end
 
 Config.maxRange[3] = -1 --infinity
